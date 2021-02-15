d2998c56811ba006e82e566208b0921259b88d00
sachinbire <71229556+sachinbire@users.noreply.github.com>
2021-02-14 20:43:14 -0500

    Add files via upload

---
+++ looping.js
@@ -0,0 +1,21 @@
+var MyLoop = []; 
+for (var i = 1000; i > 0; i -= 1) {
+	MyLoop.push(i); 
+
+
+} 
+var MyArray =[]; 
+for (var i = 10000; i < 0; i +=1) {
+	MyArray.push(i)
+	if(i = 2500) {
+		window.alert("A quarter of the way there!")}
+	if(i = 5000) {
+		window.alert("Halfway there") 
+	}
+	if(i = 10000) { 
+		window.alert("I'm done")
+	}
+}
+
+
+
